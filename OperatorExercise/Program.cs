0522d2df7ef46cc3da338f3017a36309d7154f3c
MuscuarBean <email@example.com>
2023-08-08 21:33:25 -0700

    Created operator

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,30 @@
 {
     public class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a= 17;
+            int b = 4;
+            int quotient = a/b;
+            var remainder = a % b;
+            
+          
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+            Console.WriteLine( "What is the radius of your circle:" );
+
+            var userInput = Console.ReadLine();
+            
+            var radius = double.Parse(userInput);
+
+            Console.WriteLine(AreaofCircle(radius));
         }
+
+        public static double AreaofCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+            return area;
+        }
+
     }
 }
